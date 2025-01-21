1e9f9cd7d5124769370b69a71c14985604bdc245
dzikrisyairozi <dikiogres@gmail.com>
2025-01-22 01:27:16 +0700

    chore: add eslint to find and fix problem with auto sort imports

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   output: 'export',
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: false,
+    dirs: ['app', 'components', 'lib', 'hooks'], // Directories to lint, add upon adding more files
   },
   images: { unoptimized: true },
 };
